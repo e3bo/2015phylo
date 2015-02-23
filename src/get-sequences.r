032cb952ee99a014a4c9a599558e2ae70e500806
Eamon O'Dea <odea35@gmail.com>
2015-02-23 10:51:24 -0500

    a

--- src/get-sequences.r
+++ src/get-sequences.r
@@ -2,21 +2,35 @@
 
 library(ape)
 library('rentrez')
+library(lubridate)
 
 feats <- read.csv('pedv-north-america.csv', stringsAsFactors=FALSE)
-ind <- grep(':', feats$country)
-feats <- feats[ind, ]
 foo <- strsplit(feats$country, split=':[ ]*')
-foo <- sapply(foo, '[[', 2)
+tmpf <- function(x) x[[length(x)]]
+foo <- sapply(foo, tmpf)
 foo[foo=='NorthCarolina'] <- 'North Carolina'
 foo[foo=='Tennesse'] <- 'Tennessee'
 foo[foo=='CO'] <- 'Colorado'
-stopifnot(foo %in% state.name)
+stopifnot(foo %in% c(state.name, 'Canada', 'USA', 'Mexico'))
 ind <- match(foo, state.name)
 feats$abb <- state.abb[ind]
-feats$datef <- as.Date(feats$date, '%d-%b-%Y')
-test <- !is.na(feats$datef)
-feats <- feats[test,]
+feats$loc <- ifelse(is.na(feats$abb), feats$country, feats$abb)
+
+monyr <- grepl("^\\w{3}-\\d{4}", feats$date)
+feats$precision[monyr] <- 30
+feats$tipDate[monyr] <- paste0("01-", feats$date[monyr])
+
+dmy <- grepl("^\\d{2}-\\w{3}-\\d{4}$", feats$date)
+feats$precision[dmy] <- 1
+feats$tipDate[dmy] <- feats$date[dmy]
+
+y <- grepl("^\\d{4}$", feats$date)
+foo <- feats$accession[y]
+last_collection_date_possible <- c("KJ184549.1"=ymd(20131231), "KF468754.1"=ymd(20130902), "KF468753.1"=ymd(20130902))
+## These dates based on publication history of any papers linked in genbank records
+end <- last_collection_date_possible[foo]
+feats$precision[y] <- as.duration(interval(end=end, start=ymd(20130501)))/ddays(1)
+feats$tipDate[y] <- paste0("01-May-", feats$date[y])
 
 query <- paste(feats$accession, collapse=' ')
 res <- entrez_search(db='nucleotide', term=query, retmax=100)
@@ -27,7 +41,8 @@ dna <- read.dna("pedv.fasta", "fasta")
 labs <- names(dna)
 labs <- strsplit(labs, split='\\|')
 acsn <- sapply(labs, '[[', 4)
-feats$tname <- paste(feats$datef, feats$abb, feats$accession, sep='_')
+feats$tname <- paste(feats$loc, feats$tipDate, feats$precision, feats$accession, sep='_')
 ind <- match(acsn, feats$accession)
+test <- sapply(dna, length) > 27000
 names(dna) <- feats$tname[ind]
-write.dna(dna, file='pedv-renamed.fasta', format='fasta')
+write.dna(dna[test], file='pedv-renamed.fasta', format='fasta')
