736602bb03657a4e8e7c4ceaec62f4c620a4c7dd
Eamon O'Dea <odea35@gmail.com>
2015-02-24 23:11:12 -0500

    a

--- src/make-regression-inputs.r
+++ src/make-regression-inputs.r
@@ -2,20 +2,47 @@
 
 library(ape)
 
+trRegion <- function(x) {
+    ind <- match(x, state.abb)
+    reg <- state.region[ind]
+    regF <- factor(reg,
+                   levels=c("Northeast", "South", "North Central", "West"),
+                   labels=c('A', 'T', 'C', 'G'))
+    regF <- as.character(regF)
+    regF[is.na(regF)] <- 'N'
+    regF[regF=='A'] <- 'N'
+    regF[x=='Mexico'] <- 'A' ## let mexico have northeast label since no northeast sequences
+    regF
+}
+
 tree <- read.nexus('mcc.tree')
 nms <- tree$tip.label
 abs <- sapply(strsplit(nms, '_'), '[[', 1)
-ind <- match(abs, state.abb)
-reg <- state.region[ind]
 
-regDNA <- factor(reg,
-                 levels=c("Northeast", "South", "North Central", "West"),
-                 labels=c('A', 'T', 'C', 'G'))
-regDNA <- as.character(regDNA)
-regDNA[is.na(regDNA)] <- 'N'
-regDNA[abs=='Mexico'] <- 'A'
-## let mexico have northeast label since no northeast sequences
+regDNA <- trRegion(abs)
 regDNA <- as.list(regDNA)
 names(regDNA) <- nms
+
 write.dna(regDNA, file='loc.aln', format='fasta')
 write.tree(tree, file='mcc.nh')
+save(regDNA, file='regDNA.RData')
+
+flows <- read.csv("shipment-flows-origins-on-rows-dests-on-columns.csv", row.names=1)
+                                        #TCGA
+regRow <- trRegion(rownames(flows))
+regCol <- trRegion(colnames(flows))
+
+alphOrd <- c('T', 'C', 'G', 'A')
+pairs <- expand.grid(to=alphOrd, from=alphOrd)
+test <- pairs$from != pairs$to
+pairs <- pairs[test,]
+pairs <- pairs[, c('from', 'to')]
+
+aggFlow <- function(from, to){
+    rowTest <- regRow==from
+    colTest <- regCol==to
+    log10(sum(flows[rowTest, colTest]) + 1)
+}
+pairFlows <- mapply(aggFlow, from=pairs$from, to=pairs$to)
+Z <- cbind("(Intercept)"=1, pairFlows)
+cat(t(Z), file='designMat2')
