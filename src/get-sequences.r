1e7c1a537760cfc1c86b0ed42dbb47d4421e2531
Eamon O'Dea <odea35@gmail.com>
2015-03-02 15:47:29 -0500

    begin filtering strains to field samples'

--- src/get-sequences.r
+++ src/get-sequences.r
@@ -4,6 +4,14 @@ library(ape)
 library('rentrez')
 library(lubridate)
 
+## sets of acceccsions based on information in papers
+okaCell2014field <- paste0('KM3922', 26:31 + 0.1) # field designation based on Table 1
+vlasovaDistinct2014 <- paste0('KJ645', 635:708 + 0.1)
+huangOrigin2013 <- paste0('KF46875', 2:4 + 0.1)
+marthalerComplete2013 <- 'KF272920.1'
+wangNew2014 <- 'KJ399978.1'
+## identification of 3-rd strain
+
 feats <- read.csv('pedv-north-america.csv', stringsAsFactors=FALSE)
 foo <- strsplit(feats$country, split=':[ ]*')
 tmpf <- function(x) x[[length(x)]]
@@ -16,6 +24,19 @@ ind <- match(foo, state.name)
 feats$abb <- state.abb[ind]
 feats$loc <- ifelse(is.na(feats$abb), feats$country, feats$abb)
 
+feats$isFieldSample <- NA
+feats[vlasovaDistinct2014, 'isFieldSample'] <- TRUE
+feats[okaCell2014field, 'isFieldSample'] <- TRUE
+feats[huangOrigin2013, 'isFieldSample'] <- TRUE
+feats[marthalerComplete2013, 'isFieldSample'] <- TRUE
+feats[wangNew2014, 'isFieldSample'] <- TRUE
+feats['KM392226.1','loc'] <- 'MI'
+feats['KM392227.1','loc'] <- 'MI'
+feats['KM392228.1', 'loc'] <- 'OH'
+feats['KM392229.1', 'loc'] <- 'OH'
+feats['KM392230.1', 'loc'] <- 'OH'
+feats['KM392231.1', 'loc'] <- 'OH'
+
 monyr <- grepl("^\\w{3}-\\d{4}", feats$date)
 feats$precision[monyr] <- 30
 feats$tipDate[monyr] <- paste0("01-", feats$date[monyr])
@@ -26,11 +47,13 @@ feats$tipDate[dmy] <- feats$date[dmy]
 
 y <- grepl("^\\d{4}$", feats$date)
 foo <- feats$accession[y]
-last_collection_date_possible <- c("KJ184549.1"=ymd(20131231), "KF468754.1"=ymd(20130902), "KF468753.1"=ymd(20130902))
-## These dates based on publication history of any papers linked in genbank records
+last_collection_date_possible <- c("KJ184549.1"=ymd(20131231), "KF468754.1"=ymd(20130726), "KF468753.1"=ymd(20130726))
+## These dates based on submission dates in genbank records
 end <- last_collection_date_possible[foo]
 feats$precision[y] <- as.duration(interval(end=end, start=ymd(20130501)))/ddays(1)
 feats$tipDate[y] <- paste0("01-May-", feats$date[y])
+
+
 
 query <- paste(feats$accession, collapse=' ')
 res <- entrez_search(db='nucleotide', term=query, retmax=100)
--- src/parse.py
+++ src/parse.py
@@ -11,11 +11,13 @@ for g in gb:
             if 'collection_date' in feat.qualifiers and 'country' in feat.qualifiers:
                 rec =  [str(g.id), feat.qualifiers['country'][0]]
                 rec.append(feat.qualifiers['collection_date'][0])
+                if 'strain' in feat.qualifiers:
+                    rec.append(feat.qualifiers['strain'][0])
                 rec = ','.join(rec)
                 recs.append(rec)
             
 f = open('pedv-north-america.csv', 'w')
-f.write('accession,country,date\n')
+f.write('country,date,strain\n')
 for rec in recs:
     f.write(rec + '\n')
 f.close()
