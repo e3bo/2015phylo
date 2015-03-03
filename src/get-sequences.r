d8f1c2ad490dd4b116e50470fc5bf70a27712817
Eamon O'Dea <odea35@gmail.com>
2015-03-02 23:42:11 -0500

    New selection of whole genome sequences

--- src/get-sequences.r
+++ src/get-sequences.r
@@ -4,13 +4,17 @@ library(ape)
 library('rentrez')
 library(lubridate)
 
-## sets of acceccsions based on information in papers
+## sets of accessions based on information in papers
 okaCell2014field <- paste0('KM3922', 26:31 + 0.1) # field designation based on Table 1
 vlasovaDistinct2014 <- paste0('KJ645', 635:708 + 0.1)
 huangOrigin2013 <- paste0('KF46875', 2:4 + 0.1)
 marthalerComplete2013 <- 'KF272920.1'
-wangNew2014 <- 'KJ399978.1'
-## identification of 3-rd strain
+wangNew2014 <- c('KJ399978.1', 'KJ408801.1')
+marthalerThird2014 <- 'KM077139.1'
+stevensonEmergence2013 <- c('KF452322.1', 'KF452323.1')
+chenIsolation2013noPassage <- c('KF650370.1', 'KF650373.1') 
+hoangFull2013 <- 'KF804028.1'
+wangGenbank <- 'KJ584361.1' # note in Genbank record identifies this as a case
 
 feats <- read.csv('pedv-north-america.csv', stringsAsFactors=FALSE)
 foo <- strsplit(feats$country, split=':[ ]*')
@@ -25,17 +29,32 @@ feats$abb <- state.abb[ind]
 feats$loc <- ifelse(is.na(feats$abb), feats$country, feats$abb)
 
 feats$isFieldSample <- NA
-feats[vlasovaDistinct2014, 'isFieldSample'] <- TRUE
-feats[okaCell2014field, 'isFieldSample'] <- TRUE
-feats[huangOrigin2013, 'isFieldSample'] <- TRUE
-feats[marthalerComplete2013, 'isFieldSample'] <- TRUE
-feats[wangNew2014, 'isFieldSample'] <- TRUE
+foo <- c(vlasovaDistinct2014, okaCell2014field, huangOrigin2013, marthalerComplete2013,
+         wangNew2014, marthalerThird2014, stevensonEmergence2013, chenIsolation2013noPassage,
+         hoangFull2013, wangGenbank)
+feats[foo, 'isFieldSample'] <- TRUE
+
+feats$isPassaged <- FALSE
+foo <- c(okaCell2014field, hoangFull2013)
+feats[foo, 'isPassaged'] <- TRUE
+
+
+## from table 1 of Oka et al.
 feats['KM392226.1','loc'] <- 'MI'
 feats['KM392227.1','loc'] <- 'MI'
 feats['KM392228.1', 'loc'] <- 'OH'
 feats['KM392229.1', 'loc'] <- 'OH'
 feats['KM392230.1', 'loc'] <- 'OH'
 feats['KM392231.1', 'loc'] <- 'OH'
+
+## from Genbank record
+feats['KF452322.1', 'loc'] <- 'IA'
+feats['KF452323.1', 'loc'] <- 'IN'
+
+## from publication
+feats['KF650370.1', 'loc'] <- 'IN'
+feats['KF650373.1', 'loc'] <- 'IA'
+feats[hoangFull2013, 'loc'] <- 'IA'
 
 monyr <- grepl("^\\w{3}-\\d{4}", feats$date)
 feats$precision[monyr] <- 30
@@ -46,26 +65,32 @@ feats$precision[dmy] <- 1
 feats$tipDate[dmy] <- feats$date[dmy]
 
 y <- grepl("^\\d{4}$", feats$date)
-foo <- feats$accession[y]
+foo <- rownames(feats)[y]
 last_collection_date_possible <- c("KJ184549.1"=ymd(20131231), "KF468754.1"=ymd(20130726), "KF468753.1"=ymd(20130726))
 ## These dates based on submission dates in genbank records
 end <- last_collection_date_possible[foo]
 feats$precision[y] <- as.duration(interval(end=end, start=ymd(20130501)))/ddays(1)
 feats$tipDate[y] <- paste0("01-May-", feats$date[y])
 
-
-
-query <- paste(feats$accession, collapse=' ')
-res <- entrez_search(db='nucleotide', term=query, retmax=100)
+query <- paste(rownames(feats), collapse=' ')
+res <- entrez_search(db='nucleotide', term=query, retmax=nrow(feats))
 seqs <- entrez_fetch(db='nucleotide', rettype='fasta', id=res$ids)
 write(seqs, 'pedv.fasta')
 
 dna <- read.dna("pedv.fasta", "fasta")
 labs <- names(dna)
 labs <- strsplit(labs, split='\\|')
-acsn <- sapply(labs, '[[', 4)
-feats$tname <- paste(feats$loc, feats$tipDate, feats$precision, feats$accession, sep='_')
-ind <- match(acsn, feats$accession)
+names(dna) <- sapply(labs, '[[', 4)
 test <- sapply(dna, length) > 27000
-names(dna) <- feats$tname[ind]
-write.dna(dna[test], file='pedv-renamed.fasta', format='fasta')
+
+feats$isWholeGenome <- test[rownames(feats)]
+feats$tname <- paste(feats$loc, feats$tipDate, feats$precision, rownames(feats),
+                     ifelse(feats$isPassaged, 'passaged', 'unpassaged'), sep='_')
+
+test <- feats$isFieldSample %in% TRUE & feats$isWholeGenome
+sel <- rownames(feats)[test]
+
+dna <- dna[sel]
+names(dna) <- feats[sel, 'tname']
+
+write.dna(dna, file='pedv-renamed.fasta', format='fasta')
