2bbd2b3034e9255ac8bdae1160bd251e98a4ac5a
Eamon O'Dea <odea35@gmail.com>
2015-03-16 19:12:51 -0400

    Plot convergence of ASDF toward 0,  #14

--- src/convergence-check.r
+++ src/convergence-check.r
@@ -109,18 +109,155 @@ my.gelman.plot <- function (x, bin.width = 10, max.bins = 50, confidence = 0.95,
 }
 my.gelman.plot(wml, ylim=c(1, 1.1))
 
-tfiles <- paste(runstems, '.trees', sep='')
-truns <- lapply(tfiles, read.nexus)
+## Now for the trees
 
-ntrees <- sapply(truns, length)
-stopifnot(isTRUE(all.elements.equal(ntrees)))
-ntrees <- ntrees[1]
-
-brn <- seq_len(ceiling(ntrees*burnin))
-tmpf <- function(x) {
-    x[-brn]
+phymcmc <- function(x, start=1, end=numeric(0), thin=1){
+    ## extension of mcmc to multiPhylo objects, based on code in coda 
+    if (!inherits(x, 'multiPhylo')) {
+        stop("Not an object of class multiPhylo")
+    } else {
+        niter <- length(x)
+        nms <- names(x)[1:2]
+        s12 <- as.integer(sapply(strsplit(nms, split='_'), '[[', 2))
+        if(missing(thin))
+            thin <- diff(s12)
+        if(missing(start))
+            start=s12[1]
+        if(missing(end))
+            end <- start + thin * (niter - 1)
+        nobs <- floor((end - start)/thin + 1)
+        if (niter != nobs)
+            stop("Start, end and thin incompatible with data")
+        attr(x, 'mcpar') <- c(start, end, thin)
+        attr(x, 'class') <- c('phymcmc', 'multiPhylo')
+        x
+    }
 }
-wtrns <- lapply(truns, tmpf)
+
+start.phymcmc <- function (x, ...)
+    {
+        mcpar(x)[1]
+    }
+
+end.phymcmc <- function (x, ...)
+    {
+        mcpar(x)[2]
+    }
+
+frequency.phymcmc <- function (x, ...)
+    {
+        1/thin(x)
+    }
+
+thin.phymcmc <- function (x, ...)
+    {
+        mcpar(x)[3]
+    }
+
+time.phymcmc <- function (x, ...)
+    {
+        ts(seq(from = start(x), to = end(x), by = thin(x)), start = start(x),
+           end = end(x), deltat = thin(x))
+    }
+
+window.phymcmc <- function (x, start, end, thin, ...) 
+{
+    ts.eps <- getOption("ts.eps")
+    xmcpar <- mcpar(x)
+    xstart <- xmcpar[1]
+    xend <- xmcpar[2]
+    xthin <- xmcpar[3]
+    if (missing(thin)) 
+        thin <- xthin
+    else if (thin%%xthin != 0) {
+        thin <- xthin
+        warning("Thin value not changed")
+    }
+    xtime <- as.vector(time(x))
+    if (missing(start)) 
+        start <- xstart
+    else if (length(start) != 1) 
+        stop("bad value for start")
+    else if (start < xstart) {
+        start <- xstart
+        warning("start value not changed")
+    }
+    if (missing(end)) 
+        end <- xend
+    else if (length(end) != 1) 
+        stop("bad value for end")
+    else if (end > xend) {
+        end <- xend
+        warning("end value not changed")
+    }
+    if (start > end) 
+        stop("start cannot be after end")
+    if (all(abs(xtime - start) > abs(start) * ts.eps)) {
+        start <- xtime[(xtime > start) & ((start + xthin) > xtime)]
+    }
+    if (all(abs(end - xtime) > abs(end) * ts.eps)) {
+        end <- xtime[(xtime < end) & ((end - xthin) < xtime)]
+    }
+    use <- 1:length(x)
+    use <- use[use >= trunc((start - xstart)/xthin + 1.5) & use <= 
+        trunc((end - xstart)/xthin + 1.5) & (use - trunc((start - 
+        xstart)/xthin + 1.5))%%(thin%/%xthin) == 0]
+    y <- x[use]
+    return(phymcmc(y, start = start, end = end, thin = thin))
+}
+
+print.phymcmc <- function (x, ...)
+    {
+        x.orig <- x
+        cat("Markov Chain Monte Carlo (MCMC) output:\nStart =", start(x),
+            "\nEnd =", end(x), "\nThinning interval =", thin(x), "\n")
+        attr(x, "mcpar") <- NULL
+        attr(x, "class") <- 'multiPhylo'
+        NextMethod("print", ...)
+        invisible(x.orig)
+    }
+
+is.phymcmc <- function(x) inherits(x, 'phymcmc')
+    
+phymcmc.list <- function (...)
+    {
+        x <- list(...)
+        if (length(x) == 1 && is.list(x[[1]]))
+            x <- x[[1]]
+        if (!all(unlist(lapply(x, is.phymcmc))))
+            stop("Arguments must be phymcmc objects")
+        nargs <- length(x)
+        if (nargs >= 2) {
+            xmcpar <- lapply(x, mcpar)
+            if (!all(unlist(lapply(xmcpar, "==", xmcpar[[1]]))))
+                stop("Different start, end or thin values in each chain")
+            xtlabels <- lapply(x, attr, 'TipLabel')
+            if (!all(unlist(lapply(xtlabels, "==", xtlabels[[1]]))))
+                stop("Different taxa among chains")            
+        }
+        class(x) <- "phymcmc.list"
+        return(x)
+    }
+
+start.phymcmc.list <- function (x, ...)
+    {
+        start(x[[1]])
+    }
+
+end.phymcmc.list <- function (x, ...)
+    {
+        end(x[[1]])
+    }
+
+thin.phymcmc.list <- function (x, ...)
+    {
+        thin(x[[1]])
+    }
+
+window.phymcmc.list <- function (x, ...)
+    {
+        structure(lapply(x, window.phymcmc, ...), class = "phymcmc.list")
+    }
 
 getSDSF <- function(x, minCladeFz=0.1){
     niter <- length(x[[1]])
@@ -149,7 +286,7 @@ getSDSF <- function(x, minCladeFz=0.1){
     rownames(counts) <- mg$clade
     counts[is.na(counts)] <- 0
     fz <- counts/niter
-    test <- apply(fz, 1, function(x) max(x) > minCladFz)
+    test <- apply(fz, 1, function(x) max(x) > minCladeFz)
     fz <- fz[test,]
     sdcf <- apply(fz, 1, sd)
     sdcf
@@ -161,17 +298,17 @@ asdsf.preplot <- function(x, nbin){
         stop("Less than 50 iterations in chain")
     }
     binw <- floor((niter - 50)/nbin)
-    last.iter <- c(seq(from=50, by=binw, length=nbin), niter)
+    last.iter <- c(seq(from=start(x) + 50*thin(x), by=binw * thin(x) , length=nbin), end(x))
     tmpf <- function(y) {
-        z <- lapply(x, '[', 1:y)
+        z <- window(x, end=y)
         getSDSF(z)
     }
     sdsf <- lapply(last.iter, tmpf)
     return(list(sdsfl=sdsf, last.iter=last.iter))
 }
 
-asdf.plot <- function(...){
-    ans <- asdf.preplot(...)
+asdsf.plot <- function(...){
+    ans <- asdsf.preplot(...)
     means <- sapply(ans$sdsfl, mean)
     plot(c(ans$last.iter[1], ans$last.iter), c(0, means), type='n',
          xlab='Last iteration', ylab='ASDF')
@@ -181,10 +318,18 @@ asdf.plot <- function(...){
     return(invisible(ans))
 }
 
+## Now to actually make use of all those functions
 
+tfiles <- paste(runstems, '.trees', sep='')
+truns <- lapply(tfiles, read.nexus)
+tmobs <- lapply(truns, phymcmc)
+tml <- phymcmc.list(bar)
+
+wstart <- burnin*end(tml)
+wtml <- window(tml, start=wstart)
 
 nbin <- 8
-system.time(ans <- asdsf.plot(wtrns, nbin=nbin))
+system.time(ans <- asdsf.plot(wtml, nbin=nbin))
 hist(log10(ans$sdsfl[[nbin]]))
 
 save.image('convergence-check.RData')
