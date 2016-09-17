de43b8a399ccd6ae491962e245715d7d66979be7
Eamon O'Dea <odea35@gmail.com>
2016-09-16 21:07:31 -0400

    Add test of consistency with TreePar

---
+++ src/penaltree/tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(penaltree)
+
+test_check("penaltree")
---
+++ src/penaltree/tests/testthat/test-likelihood.R
@@ -0,0 +1,32 @@
+set.seed(3)
+
+context("birth-death process")
+
+test_that("Birth-death likelihood is consistent with TreePar function", {
+    skip_if_not_installed("TreePar")
+    skip_if_not_installed("TreeSim")
+
+    lambda11 <- 15
+    lambda12 <- 3
+    lambda21 <- 1
+    lambda22 <- 3
+    death1 <- 4
+    death2 <- 4
+    sampprob1 <- 0.05
+    sampprob2 <- 0.05
+    l <- rbind(c(lambda11, lambda12), c(lambda21, lambda22))
+    d <- c(death1, death2)
+    s <- c(sampprob1, sampprob2)
+    n <- 20
+    init <- -1
+     
+    tree <- TreeSim::sim.bdtypes.stt.taxa(n, l, d, s, init)
+    tree <- TreePar::addroot(tree, tree$root.edge)
+    par <- c(2, 2, 2, 2)
+    fix <- rbind(c(1, 6, 7, 8), c(15, -5, 0, 0), c(1, 1, 1, 1))
+    tplik <- TreePar::LikTypesSTT(par=par, phylo=tree, fix=fix, sampfrac=s,
+                                  survival=0, posR=0)
+    ptlik <- mylik(par=par, phylo=tree, fix=fix, sampfrac=s, survival=0,
+                   supercrit=0)
+    expect_equal(tplik, ptlik)
+})
