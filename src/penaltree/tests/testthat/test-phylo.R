f1859120b820f4dd2fa7ccfdf3308077eb9d5684
Eamon O'Dea <odea35@gmail.com>
2016-11-09 22:02:08 -0500

    Eliminate extra parameters with 0 deg. of freedom
    Fourth base frequency and final subtitution model parameter in GTR model

--- src/penaltree/tests/testthat/test-phylo.R
+++ src/penaltree/tests/testthat/test-phylo.R
@@ -433,7 +433,7 @@ test_that(paste("Able to estimate parameters given HYK subs model + Gamma4",
     expect_equal(alpha_est, alpha, tol=.5)
 })
 
-test_that(paste("Able to estimate parameters given HYK subs model + Gamma4",
+test_that(paste("Able to estimate parameters given GTR subs model + Gamma4",
                 "with inferred topology"),{
     skip_if_not_installed("phangorn")
     skip_if_not_installed("ape")
@@ -445,8 +445,10 @@ test_that(paste("Able to estimate parameters given HYK subs model + Gamma4",
                                  nh$tipheights[tree_time$tip.label], sep="_")
     names(nh$tipheights) <- paste(names(nh$tipheights), nh$tipheights, sep="_")
 
-    kappa <- 4
-    bf <- c(A=.25, C=.25, G=.1, T=.4)
+    subs_params <- c(AC=.2, AG=.8, AT=.14, CG=.9, CT=.1, GT=.12)
+    subs_params <- subs_params / subs_params[6]
+    bf <- c(.2, .14, .25, .4)
+    bf <- bf / sum(bf)
 
     alpha <- 0.5
     nrates <- 4
@@ -458,10 +460,10 @@ test_that(paste("Able to estimate parameters given HYK subs model + Gamma4",
     tree_subs$edge.length <- tree_subs$edge.length * subs_per_time
 
     tree_char <- ape::write.tree(tree_subs)
-    true_tm <- rphast::tm(tree_char, subst.mod="HKY85", backgd = bf,
+    true_tm <- rphast::tm(tree_char, subst.mod="REV", backgd = bf,
                           nratecats = nrates, rate.consts = rate.consts,
                           rate.weights = rate.weights)
-    true_tm <- rphast::set.rate.matrix.tm(true_tm, params=kappa)
+    true_tm <- rphast::set.rate.matrix.tm(true_tm, params=subs_params)
     ncols <- 1e2
     sim <- rphast::simulate.msa(true_tm, ncols)
 
@@ -478,33 +480,32 @@ test_that(paste("Able to estimate parameters given HYK subs model + Gamma4",
 
     obj <- function(x) {
         subs_per_time <- x[1]
-        pi <- x[seq(2, 5)]
-        pi <- pi / sum(pi)
+        pi <- x[seq(2, 4)]
+        pi <- c(pi, 1) / (1 + sum(pi))
         names(pi) <- c("A", "C", "G", "T")
-        subs_pars <- x[seq(6, 11)]
-        alpha <- x[12]
-        node_times <- x[seq(13, length(x))]
+        subs_pars <- c(x[seq(5, 9)], 1)
+        alpha <- x[10]
+        node_times <- x[seq(11, length(x))]
         lmsa_wrapper(tree_inf, node_times = node_times, tip_times = nh$tip,
                      msa = sim, subs_per_time = subs_per_time, alpha = alpha,
                      subs_model = "REV", nrates = 4,
-                     subs_pars = kappa, pi = pi)
+                     subs_pars = subs_pars, pi = pi)
     }
-    init <- c(temp_ests$subs_per_time, rate_ests$bf,
-              rate_ests$gtr_pars, rate_ests$alpha, nh$node)
+    init <- c(temp_ests$subs_per_time, rate_ests$bf[-4] / rate_ests$bf[4],
+              rate_ests$gtr_pars[-6], rate_ests$alpha, nh$node)
     ans <- rphast::optim.rphast(obj, init, lower=rep(0, length(init)),
                                  logfile="/tmp/optim.log")
-    nhest <- ans$par[-seq(1, 12)]
+    nhest <- ans$par[-seq(1, 10)]
     tree_est <- set_branchlengths(tree_inf, nodeheights = nhest,
                                   tipheights = nh$tip)$tree
-
-    kappa_est <- ans$par[6]
-    alpha_est <- ans$par[7]
-    bf_est <- ans$par[seq(2, 5)]
+    subs_pars_est <-  c(ans$par[seq(5, 9)], 1)
+    alpha_est <- ans$par[10]
+    bf_est <- c(ans$par[seq(2, 4)], 1)
     bf_est <- bf_est / sum(bf_est)
 
     expect_equal(sort(nhest), sort(nh$node), tol = .5)
     expect_equal(ans$par[1], subs_per_time, tol=.5)
     expect_equal(bf_est, unname(bf), tol=.5)
-    expect_equal(kappa_est, kappa, tol=.5)
+    expect_equal(subs_pars_est[-6], unname(subs_params)[-6], tol=.5)
     expect_equal(alpha_est, alpha, tol=.5)
 })
