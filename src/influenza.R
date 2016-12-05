3a8e4fe1abbea81ed67aa13c32eeec84b23976bd
Eamon O'Dea <odea35@gmail.com>
2016-12-05 08:26:21 -0500

    Flu analysis running with new optimizers

--- src/influenza.R
+++ src/influenza.R
@@ -62,27 +62,26 @@ pf1 <- c(0, 0, rep(1, 10))
 pars <- pm1(x = x2, w = init1)
 
 tree_timel <- lapply(tree_info, "[[", "tree_time")
-ncpu <- max(parallel::detectCores() - 1, 20)
-sp <- penaltree:::stabpath_gpnet(x = x2, y = tree_timel[1],
+ncpu <- min(parallel::detectCores() - 1, 20)
+sp <- penaltree::stabpath_gpnet(x = x2, y = tree_timel[1],
                calc_convex_nll = penaltree::calc_bd_lm_nll,
                param_map = pm1, nlambda = 10, lambda.min.ratio = 0.1,
                make_log = TRUE, penalty.factor = pf1,
                thresh = 1e-3, winit = init1, alpha = 1,
                steps=20, mc.cores = ncpu)
 
-
-
 spstats <- plot(sp)
 
 xstable <- x2[, spstats$stable - 2, drop=FALSE]
+stopifnot(ncol(xstable) == 1)
 
 pf2 <- c(0, 0, 100, 100, 1)
 init2 <- init1[c(seq(1, 4), spstats$stable + 4)]
 selected_fit <- penaltree::get_gpnet(x = xstable, y = tree_timel[1],
                calc_convex_nll = penaltree::calc_bd_lm_nll,
                param_map = pm1, nlambda = 10, lambda.min.ratio = 0.01,
-               verbose = FALSE, penalty.factor = pf2,
-               thresh = 1e-4, winit = init2, alpha = 1)
+               make_log = TRUE, penalty.factor = pf2,
+               thresh = 1e-3, winit = init2, alpha = 1)
 
 sel_coef <- c(selected_fit$a0[, 10], selected_fit$beta[, 10])
 sel_par <- pm1(xstable, sel_coef)
@@ -94,29 +93,25 @@ sim_tree <- penaltree::sim_bd_proc(n = nsamples, l = sel_par$l,
 # do stability selection and fitting of simulated tree
 
 init3 <- init1
-init3[c(1,2)] <- c(0.8, 0)
 sp_sim <- penaltree::stabpath_gpnet(x = x2, y = list(sim_tree),
                calc_convex_nll = penaltree::calc_bd_lm_nll,
-               param_map = pm1, nlambda = 10, lambda.min.ratio = 0.25,
+               param_map = pm1, nlambda = 10, lambda.min.ratio = 0.01,
                make_log = TRUE, penalty.factor = pf1,
-               thresh = 1e-4, winit = init3, alpha = 1,
-               steps=20, mc.cores = 20)
+               thresh = 1e-3, winit = init3, alpha = 1,
+               steps = 20, mc.cores = ncpu)
 
 spstats_sim <- plot(sp_sim)
 
 stopifnot(isTRUE(all.equal(spstats_sim$stable, spstats$stable)))
 
-set.seed(3)
 sim_fit <- penaltree::get_gpnet(x = xstable, y = list(sim_tree),
                calc_convex_nll = penaltree::calc_bd_lm_nll,
-               param_map = pm1, nlambda = 100, lambda.min.ratio = 0.01,
+               param_map = pm1, nlambda = 10, lambda.min.ratio = 0.01,
                make_log = TRUE, penalty.factor = pf2,
                thresh = 1e-3, winit = init2, alpha = 1)
 
-
 save.image("influenza.RData")
-
-q('no')
+q("no")
 
 ## plot ll surface
 
@@ -152,18 +147,12 @@ out2 <- penaltree::get_gpnet(x = x2, y = tree_timel[2], calc_convex_nll = calc_b
                   verbose = TRUE, penalty.factor = pf1,
                   thresh = 1e-4, winit = init1, alpha = 1)
 
-
-
-obj <- function(x, ind=9) {
-    foo <- c(sim_fit$a0[, ind], sim_fit$beta[, ind])
-    foo[5] <- x
-    calc_bd_lm_nll(w=foo, x=xstable, y=list(sim_tree), param_map=pm1)
+obj <- function(x) {
+    foo[c(1,2)] <- x
+    l <- calc_bd_lm_nll(w=foo, x=x2, y=tree_timel[1], param_map=pm1)
+    print(paste(c(x, l)))
+    l
 }
-prof9 <- sapply(seq(2, 3, .1), obj)
-
-
-
-
 ans <- optim.rphast(obj, c(1, -.1), lower=c(-1,-1), upper=c(1, 0.5), logfile="/tmp/a.log")
 
 
@@ -196,29 +185,3 @@ out <- get_gpnet(x = x2, y = small_treel, calc_convex_nll=penaltree::calc_bd_lm_
                  param_map=pm, lambda=seq(10, 1),
                  make_log = TRUE, penalty.factor=c(0, 0, rep(1,12)),
                  thresh=1e-4, winit=init, alpha=1)
-
-calc_fmod <- function(d){
-    par2 <- d + par
-    d %*% (H/2) %*% d + gnll %*% d + F1 - penFunc(par) + penFunc(par2)   
-}
-
-wrap_mod <- function(x){
-    d <- rep(0, 5)
-    d[2] <- x
-    calc_fmod(d)
-}
-
-d2seq <- seq(1e-4, -1e-4, len=3)
-d2pref <- sapply(d2seq, wrap_mod)
-
-
-wrap_obj <- function(x){
-    d <- rep(0, 5)
-    d[2] <- x
-    par2 <- par + d
-    nll(par2) + penFunc(par2)
-}
-d2true <- sapply(d2seq, wrap_obj)
-
-plot(d2seq, d2true, col=2)
-plot(d2seq, d2pref)
