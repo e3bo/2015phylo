65d0dee1a869d96f0cf3e910804fe9cad2ebd504
Eamon O'Dea <odea35@gmail.com>
2017-11-23 23:03:29 -0500

    Start layer 2

--- src/layered-lik.R
+++ src/layered-lik.R
@@ -25,12 +25,26 @@ ans1 <- solve_lik_unsampled(init = init, l = l, m = m, psi = psi, times = times,
 
 layered_lik <- function(init, l, m, psi, times) {
     times <-  sort(times)[-1] - min(times)
-    C0 <- m / (sum(l) + m + psi)
+    a <- sum(l) + m + psi
+    C0 <- m / a    
     C1 <- 1 - C0
-    p <- C1 * exp(-(sum(l) + m + psi) * times) + C0
+    p <- C1 * exp(-a * times) + C0
     names(p) <- times
-    p
+
+    F <- function(t) C1 ^ 2 * exp(-a * t * 3) + 2 * C0 * C1 * exp(-a * t * 2) + C0 ^ 2 * exp(-a * t)
+    p2 <- integrate(F, 1, times)$value * l[1,1] * exp(a * times)
+    
+    p2b <-  C1 ^ 2 * exp(-a * times * 3) / (-3 * a) + 2 * C0 * C1 * exp(-a * times * 2) / (-a * 2) + C0 ^ 2 * exp(-a * times) / (-a) + C1 ^ 2 * exp(-a * 3)/(3 * a) + 2 * C0 * C1 * exp(-a * 2) / (2 * a) + C0 ^ 2 * exp(-a ) / a
+    p2b <- p2b * l[1,1] * exp(a * times)
+    print(p2b)
+    stopifnot(isTRUE(all.equal(p2, p2b)))
+    p + p2b
 }
 
+ans2 <- layered_lik(init = init, l = l, m = m, psi = psi, times = times)
+all.equal(ans1, ans2, check.attributes = FALSE)
+
+l <- matrix(.001, ncol=1)
+ans1 <- solve_lik_unsampled(init = init, l = l, m = m, psi = psi, times = times, rtol = rtol, atol = atol)
 ans2 <- layered_lik(init = init, l = l, m = m, psi = psi, times = times)
 all.equal(ans1, ans2, check.attributes = FALSE)
