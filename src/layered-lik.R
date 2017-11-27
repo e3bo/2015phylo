a875bceb6f536452a1259de776b9779aed30a87f
Eamon O'Dea <odea35@gmail.com>
2017-11-26 21:32:24 -0500

    Start using CAS

--- src/layered-lik.R
+++ src/layered-lik.R
@@ -23,27 +23,24 @@ rtol <- atol <- 1e-12
 times <- c(0, 2)
 ans1 <- solve_lik_unsampled(init = init, l = l, m = m, psi = psi, times = times, rtol = rtol, atol = atol)
 
+library(rSymPy)
+
 layered_lik <- function(init, l, m, psi, times) {
     times <-  sort(times)[-1] - min(times)
     a <- -(sum(l) + m + psi)
     C0 <- -m / a
     C1 <- 1 - C0
     p <- C1 * exp(a * times) + C0
-    names(p) <- times
-
-    F <- function(t) C1 ^ 2 * exp(a * t)  + 2 * C0 * C1  + C0 ^ 2 * exp(-a * t)
-    p2 <- integrate(F, 1, times)$value * l[1,1] * exp(a * times)
-    p2b <- C1 ^ 2 / a * exp(a * times) + 2 * C0 * C1 * times + C0 ^ 2 * exp(-a * times) / -a - C1 ^ 2 * exp(a) / a - 2 * C0 * C1 - C0 ^2 * exp(-a) / -a
-    p2b <- p2b * l[1,1] * exp(a * times)
-    print(p2b)
-    stopifnot(isTRUE(all.equal(p2, p2b)))
-    p + p2b
+    Var("xi, t, C1, C0, a, l")
+    f <- Sym("integrate( (C1 * exp(a * xi) + C0)**2, (xi, 1, t)) * l * exp(-a * t)")
+    p2 <- sympy("(", f, ").subs(l, ",  as.numeric(l), ").subs(a,", a, ").subs(C0,", C0, ").subs(C1,", C1, ").subs(t,", times, ").evalf()")
+    p + as.numeric(p2)
 }
 
 ans2 <- layered_lik(init = init, l = l, m = m, psi = psi, times = times)
 all.equal(ans1, ans2, check.attributes = FALSE)
 
-l <- matrix(.01, ncol=1)
+l <- matrix(.001, ncol=1)
 ans1 <- solve_lik_unsampled(init = init, l = l, m = m, psi = psi, times = times, rtol = rtol, atol = atol)
 ans2 <- layered_lik(init = init, l = l, m = m, psi = psi, times = times)
 all.equal(ans1, ans2, check.attributes = FALSE)
