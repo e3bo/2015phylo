fae0a235e965d59c04a8b301b4b8b743891556b3
Eamon O'Dea <odea35@gmail.com>
2018-01-17 21:22:35 -0500

    Encapsulate cas code

--- src/layered-lik.R
+++ src/layered-lik.R
@@ -25,14 +25,8 @@ ans1 <- solve_lik_unsampled(init = init, l = l, m = m, psi = psi, times = times,
 
 library(rSymPy)
 
-layered_lik <- function(init, l, m, psi, times, nlayers = 1) {
-    times <-  sort(times)[-1] - min(times)
-    a <- -(sum(l) + m + psi)
-    C0 <- -m / a
-    C1 <- 1 - C0
-    num_eval <- function(exprstr) {
-        eval(parse(text = exprstr))
-    }
+
+get_approx <- function(nlayers = 1){
     Var("xi, t, C1, C0, a, l")
     p <- sympy("C1 * exp(a * t) + C0")
     get_tdf <- function(f1, f2) {
@@ -43,14 +37,30 @@ layered_lik <- function(init, l, m, psi, times, nlayers = 1) {
         p2 <- sympy("integrate(exp(-a * xi) * ", tdf, ", (xi, 1, t)) * l * exp(a * t)")
         tdf <- get_tdf(p2, p2)
     }
+    list(p, p2)
+}
+
+
+
+layered_lik <- function(init, l, m, psi, times, app) {
+    times <-  sort(times)[-1] - min(times)
+    a <- -(sum(l) + m + psi)
+    C0 <- -m / a
+    C1 <- 1 - C0
+    num_eval <- function(exprstr) {
+        eval(parse(text = exprstr))
+    }
     t <- times
-    num_eval(p) + num_eval(p2)
+    num_eval(app[[1]]) + num_eval(app[[2]])
 }
 
 ans2 <- layered_lik(init = init, l = l, m = m, psi = psi, times = times)
 all.equal(ans1, ans2, check.attributes = FALSE)
 
 l <- matrix(.01, ncol=1)
-ans1 <- solve_lik_unsampled(init = init, l = l, m = m, psi = psi, times = times, rtol = rtol, atol = atol)
-system.time(ans2 <- layered_lik(init = init, l = l, m = m, psi = psi, times = times, nlayers = 2))
+system.time(ans1 <- solve_lik_unsampled(init = init, l = l, m = m, psi = psi, times = times, rtol = rtol, atol = atol))
+
+system.time(app <- get_approx(nlayers = 2))
+system.time(ans2 <- layered_lik(init = init, l = l, m = m, psi = psi, times = times, app = app))
+
 all.equal(ans1, ans2, check.attributes = FALSE)
