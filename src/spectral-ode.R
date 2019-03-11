062ed3c66b03fee157cc464572f75c207c5d3c33
Eamon O'Dea <odea35@gmail.com>
2019-03-11 16:29:24 -0400

    Implement simple spectral ode solver

--- src/spectral-ode.R
+++ src/spectral-ode.R
@@ -21,3 +21,36 @@ cheb <- function(N) {
   D <- D - diag(rowSums(D))
   list(D = D, x = x)
 }
+
+p13 <- function() {
+  ## solve linear BVP u_xx = exp(4x), u(-1)=u(1)=0
+  ##
+  ## Also plots the polynomial interpolation and the maximum difference with the exact solution.
+  ##
+  ## Value:
+  ##   vector containing u, the value of the soluation on a Chebysev grid of 17 points
+
+N <- 16
+chebN <- cheb(N)
+D2 <- chebN$D %*% chebN$D
+D2 <- D2[2:N, 2:N]
+f <- exp(4 * chebN$x[2:N])
+u <- solve(D2, f)
+u <- c(0, u, 0)
+
+plot(chebN$x, u, pch = 16, ylab = "", xlab = "", xaxs = "i", yaxs = "i",
+     ylim = c(-2.5, 0.5), tcl = 0.5, xpd = TRUE, col = "darkblue")
+grid()
+axis(3, tcl = 0.5, labels = FALSE)
+axis(4, tcl = 0.5, labels = FALSE)
+z <- poly(chebN$x, N)
+pf <- lm(u~ z)
+xx <-  seq(-1, 1, 0.01)
+zz <- predict(z, xx)
+uu <- cbind(1, zz) %*% coef(pf)
+lines(xx, uu, col = "darkblue")
+exact <- (exp(4*xx) - sinh(4)*xx - cosh(4) )/16
+lin <- signif(norm(uu - exact, type = "I"), 4)
+mtext(paste0("max err = ", lin))
+u
+}
