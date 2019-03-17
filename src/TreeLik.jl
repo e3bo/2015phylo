3f6ef2b155aedbde0de922389c5d8e4b0e2ef9fd
Eamon O'Dea <odea35@gmail.com>
2019-03-17 00:17:11 -0400

    Check deriv for nonhomogenous ode

--- src/TreeLik.jl
+++ src/TreeLik.jl
@@ -2,8 +2,15 @@ module TreeLik
 using ApproxFun
 using LinearAlgebra
 
-a,b = 0,10
+export r
+export mu
+export tau
+
+tau = 10
+a,b = 0,tau
 x = Fun(identity, a..b)
+
+
 
 d = domain(x)
 D = Derivative(d)
@@ -12,7 +19,16 @@ L = D + r * I
 
 u = [Evaluation(0); L] \ [1, 0]
 
-dLdr = Conversion(Chebyshev(0..10), Ultraspherical(1,0..10))
+dLdr = Conversion(Chebyshev(0..tau), Ultraspherical(1,0..tau))
 dudr = [Evaluation(0); L] \ (-[0 * Evaluation(0); dLdr] * u)
+
+mu = 0.2
+
+u1 = [Evaluation(0); L] \ [1, mu]
+
+du1dr = [Evaluation(0); L] \ (-[0 * Evaluation(0); dLdr] * u1)
+
+
+
 
 end
--- src/TreeLikTst.jl
+++ src/TreeLikTst.jl
@@ -1,12 +1,18 @@
 module TreeLikTst
 include("TreeLik.jl")
-import .TreeLik
+using .TreeLik
 using Test
 using ApproxFun
 using LinearAlgebra
 
-uexact = Fun(x->exp(-2x),  0..10)
+uexact = Fun(x->exp(-2x),  0..tau)
 exptest = @test norm(TreeLik.u - uexact, Inf) < 1e-14
-dexptest = @test TreeLik.dudr(10)  ≈ -10 * exp(-2 * 10)
+dexptest = @test TreeLik.dudr(10)  ≈ -tau * exp(-2 * tau)
+
+u1exact = Fun(x->exp(-2x) + 0.2, 0..10)
+u1valtest = @test norm(TreeLik.u1 - u1exact, Inf) < 1e-14
+du1drtest = @test TreeLik.du1dr(10)  ≈ -tau * exp(-r * tau) + mu / r^2 * (r * (tau * exp(-r * tau)) - (1 - exp(-r * tau))) 
+
+
 
 end
