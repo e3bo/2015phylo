e3a008ad5e6f4490e573e5f96f82430132721cac
Eamon O'Dea <odea35@gmail.com>
2019-03-17 12:25:07 -0400

    Write subsitution function for buggy norm(, Inf)

--- src/TreeLikTst.jl
+++ src/TreeLikTst.jl
@@ -6,14 +6,20 @@ using ApproxFun
 using LinearAlgebra
 
 normtol = 1e-10
+uexact = Fun(x-> exp(-r * x), 0..tau)
+u1exact = Fun(x->exp(-r * x) + (1 - exp( -r * x)) * mu / r, 0..tau)
+u1wrong = Fun(x->exp(-r * x) + (1 - exp( -r * tau)) * mu / r, 0..tau)
+
+pinfnorm(x) = maximum(abs(x)) # norm(x, Inf) is not working as of 2019 March 17
+
 layer1tests = @testset "test likelihood and gradient for single layer" begin
-    uexact = Fun(x->exp(-r * x),  0..tau)
-    exptest = @test norm(TreeLik.u - uexact, Inf)  ≈ 0 atol = normtol
-    @test TreeLik.dudr(tau)  ≈ -tau * exp(-r * tau)
-    u1exact = Fun(x->exp(-r * x) + (1 - exp( -r * tau)) * mu / r, 0..tau)
-    @test norm(TreeLik.u1 - u1exact, Inf)  ≈ 0 atol = normtol
-    @test TreeLik.du1dr(tau)  ≈ -tau * exp(-r * tau) + mu / r^2 * (r * (tau * exp(-r * tau)) - (1 - exp(-r * tau)))
-    @test TreeLik.dudmu(tau)  ≈ (1 - exp(-r * tau)) / r
+    @test pinfnorm(TreeLik.u - uexact)  ≈ 0 atol = normtol
+    @test TreeLik.dudr(tau)  ≈ -tau * uexact(tau) atol = normtol
+    @test pinfnorm(TreeLik.u1 - u1exact) ≈ 0 atol = normtol
+    @test pinfnorm(TreeLik.u1 - u1wrong) ≉ 0 atol = normtol
+    @test TreeLik.u1(1) ≉ u1wrong(1) atol = normtol
+    @test TreeLik.du1dr(tau) ≈ -tau * exp(-r * tau) + mu / r^2 * (r * (tau * exp(-r * tau)) - (1 - exp(-r * tau))) atol = normtol
+    @test TreeLik.dudmu(tau) ≈ (1 - exp(-r * tau)) / r atol = normtol
 end;
 
 end
