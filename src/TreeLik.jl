8382eb60d6a5d70bbf46abed462bd18993d50fcc
Eamon O'Dea <odea35@gmail.com>
2019-03-18 12:19:55 -0400

    Calculating gradient for layer 2

--- src/TreeLik.jl
+++ src/TreeLik.jl
@@ -2,7 +2,7 @@ module TreeLik
 using ApproxFun
 using LinearAlgebra
 
-export r, λ, ψ, μ, τ 
+export r, λ, ψ, μ, τ
 
 τ = 10
 a,b = 0,τ
@@ -17,21 +17,22 @@ r = λ + ψ + μ
 
 L = D + r * I
 u = [Evaluation(0); L] \ [1, 0]
-dLdr = Conversion(Chebyshev(0..τ), Ultraspherical(1,0..τ))
-dudr = [Evaluation(0); L] \ (-[0 * Evaluation(0); dLdr] * u)
+dLdψ = Conversion(Chebyshev(0..τ), Ultraspherical(1,0..τ))
+dudψ = [Evaluation(0); L] \ (-[0 * Evaluation(0); dLdψ] * u)
 dudmu = [Evaluation(0); L] \ [0, 1]
-dudmunew = [Evaluation(0); L] \(-[0 * Evaluation(0); dLdr] * u + [0; 1])
+dudμ = [Evaluation(0); L] \(-[0 * Evaluation(0); dLdψ] * u + [0; 1])
 
 u1 = [Evaluation(0); L] \ [1, μ]
-du1dr = [Evaluation(0); L] \ (-[0 * Evaluation(0); dLdr] * u1)
+du1dμ = [Evaluation(0); L] \(-[0 * Evaluation(0); dLdψ] * u1 + [0; 1])
+du1dψ = [Evaluation(0); L] \ (-[0 * Evaluation(0); dLdψ] * u1)
 
 L1 = L - λ * u1
+dL1dψ = dLdψ - λ * du1dψ
 u2 = [Evaluation(0); L1] \ [1, μ]
-
-du2dpsi = [Evaluation(0); L] \ (-[0 * Evaluation(0); dLdr] * u2)
-
-du2dmu = [Evaluation(0); L1] \ [0, 1]
-
-
+du2dψ = [Evaluation(0); L1] \ (-[0 * Evaluation(0); dL1dψ] * u2)
+dL1dμ = dLdψ - λ * du1dμ
+du2dμ = [Evaluation(0); L1] \ (-[0 * Evaluation(0); dL1dμ] * u2 + [0, 1])
+dL1dλ = dL1dψ - u1
+du2dλ = [Evaluation(0); L1] \ (-[0 * Evaluation(0); dL1dλ] * u2)
 
 end
--- src/TreeLikTst.jl
+++ src/TreeLikTst.jl
@@ -16,20 +16,21 @@ pinfnorm(x) = maximum(abs(x)) # norm(x, Inf) is not working as of 2019 March 17
 
 layer1tests = @testset "test likelihood and gradient for single layer" begin
     @test pinfnorm(TreeLik.u - uexact)  ≈ 0 atol = normtol
-    @test TreeLik.dudr(τ)  ≈ -τ * uexact(τ) atol = normtol
+    @test TreeLik.dudψ(τ)  ≈ -τ * uexact(τ) atol = normtol
     @test pinfnorm(TreeLik.u1 - u1exact) ≈ 0 atol = normtol
     @test pinfnorm(TreeLik.u1 - u1wrong) ≉ 0 atol = normtol
     @test TreeLik.u1(1) ≉ u1wrong(1) atol = normtol
-    @test TreeLik.du1dr(τ) ≈ -τ * exp(-r * τ) +  μ / r^2 * (r * (τ * exp(-r * τ)) - (1 - exp(-r * τ))) atol = normtol
-    @test TreeLik.dudmu(τ) ≈ (1 - exp(-r * τ)) / r atol = normtol
+    @test TreeLik.du1dψ(τ) ≈ -τ * exp(-r * τ) +  μ / r^2 * (r * (τ * exp(-r * τ)) - (1 - exp(-r * τ))) atol = normtol
+    @test TreeLik.du1dμ(τ) ≈ -τ * exp(-r * τ) + (1 - exp(-r * τ)) / r + μ / r^2 * (r * τ * exp(-r * τ) - (1 - exp(-r * τ))) atol = normtol
 end;
 
 using ParameterizedFunctions
 using DifferentialEquations
+using ForwardDiff, DiffEqSensitivity, Calculus
 
 f = @ode_def begin
   du1 = -(λ + ψ + μ) * u1 + μ
-  du2 = -(λ + ψ + μ) * u2 + λ * u1 * u2 + μ 
+  du2 = -(λ + ψ + μ) * u2 + λ * u1 * u2 + μ
 end  λ ψ μ
 
 u0 = [1.0;1.0]
@@ -42,23 +43,18 @@ function test_f(p)
   solve(_prob,Vern9(),abstol=1e-14,reltol=1e-14,save_everystep=false)[end]
 end
 
-using ForwardDiff, DiffEqSensitivity, Calculus
 fd_res = ForwardDiff.jacobian(test_f,p)
-
 calc_res = Calculus.finite_difference_jacobian(test_f, p)
-
-#using DiffResults
-#res = DiffResults.JacobianResult(u0,p) # Build the results object
-#DiffResults.jacobian!(res,p) # Populate it with the results
-#val = DiffResults.value(res) # This is the sol[end]
-#jac = DiffResults.jacobian(res) 
-
 sol = solve(prob)
 
-
-
 layer2tests = @testset "test likelihood and gradient for two layers" begin
-    @test TreeLikTst.TreeLik.u2.(sol.t) ≈ sol[2,:] rtol = 1e-4
+    @test TreeLik.u2.(sol.t) ≈ sol[2,:] rtol = 1e-4
+    @test TreeLik.du1dψ(τ) ≈ fd_res[1,1] rtol = 1e-4
+    @test TreeLik.du1dψ(τ) ≈ fd_res[1,2] rtol = 1e-4
+    @test TreeLik.du1dμ(τ) ≈ fd_res[1,3] rtol = 1e-4
+    @test TreeLik.du2dλ(τ) ≈ fd_res[2,1] rtol = 1e-4
+    @test TreeLik.du2dψ(τ) ≈ fd_res[2,2] rtol = 1e-4
+    @test TreeLik.du2dμ(τ) ≈ fd_res[2,3] rtol = 1e-4
 end;
 
 end
